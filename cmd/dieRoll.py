d6c3ed577ef39e76f9bed7fa02fa976a708ead37
Brady-Lemaster <130093018+Brady-Lemaster@users.noreply.github.com>
2023-05-09 15:22:11 -0500

    Update dieRoll.py

--- cmd/dieRoll.py
+++ cmd/dieRoll.py
@@ -1,9 +1,9 @@
-import random
-def x(dice):
-  inputList=[]
-  outputList=[]
-  for i in dice.split('d'): 
-    inputList.append(i)
-  for i in range(int(inputList[0])):
-    outputList.append(random.randint(1,int(inputList[1])))
-  return outputList
+import random # import random module for dice rolling
+def x(dice): # defines function with input variable dice
+  inputList=[] # defines the list for data to import to the roller
+  outputList=[] # defines the list for the dice rolls to be appended to
+  for i in dice.split('d'): # splits the "d" between the dice amount and dice sides
+    inputList.append(i) # append the dice amount and sides to a list for rolling
+  for i in range(int(inputList[0])): # loops for the amount of dice
+    outputList.append(random.randint(1,int(inputList[1]))) # appends the dice rolls to the output list
+  return outputList # exports the output list to the main script
