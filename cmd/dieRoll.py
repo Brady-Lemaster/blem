728be13f98a811e605eb71426186c1a3d0e32bfe
Brady-Lemaster <130093018+Brady-Lemaster@users.noreply.github.com>
2023-05-08 19:10:36 -0500

    Update dieRoll.py

--- cmd/dieRoll.py
+++ cmd/dieRoll.py
@@ -1,1 +1,9 @@
-
+import random
+def x(dice):
+  inputList=[]
+  outputList=[]
+  for i in dice.split('d'): 
+    inputList.append(i)
+  for i in range(int(inputList[0])):
+    outputList.append(random.randint(1,int(inputList[1])))
+  return outputList
