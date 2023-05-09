ea513f66e7aea8b26b2fb67c63599d3b465d6ab8
Brady-Lemaster <130093018+Brady-Lemaster@users.noreply.github.com>
2023-05-08 19:11:27 -0500

    Update package.py

--- package.py
+++ package.py
@@ -1,1 +1,8 @@
-
+if not name:
+  name = os.path.basename(uri).lower().rstrip('.py')
+r = requests.get(uri)
+r.raise_for_status()
+codeobj = compile(r.content, uri, 'exec')
+module = imp.new_module(name)
+exec (codeobj, module.__dict__)
+return module
