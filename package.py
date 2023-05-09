2d2a382e119215e4ca03b8032650814e04481817
Brady-Lemaster <130093018+Brady-Lemaster@users.noreply.github.com>
2023-05-08 20:19:10 -0500

    Update package.py

--- package.py
+++ package.py
@@ -1,6 +1,7 @@
 import os
-import imp
 import requests
+import types
+types.ModuleType('name')
 class importModule:
   def x(uri, name=None):
     if not name:
@@ -8,7 +9,7 @@ class importModule:
     r = requests.get(uri)
     r.raise_for_status()
     codeobj = compile(r.content, uri, 'exec')
-    module = imp.new_module(name)
+    module = types.ModuleType(name)
     exec (codeobj, module.__dict__)
     return module
 dieRoll = importModule.x('https://raw.githubusercontent.com/Brady-Lemaster/blem/main/cmd/dieRoll.py')
