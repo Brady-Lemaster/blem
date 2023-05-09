f8cf81d4ddd414378fcd922ffb10c2b37c0633c3
Brady-Lemaster <130093018+Brady-Lemaster@users.noreply.github.com>
2023-05-08 19:17:28 -0500

    Update package.py

--- package.py
+++ package.py
@@ -1,14 +1,22 @@
 import os
 import imp
 import requests
-def importModule(uri, name=None):
-  if not name:
-    name = os.path.basename(uri).lower().rstrip('.py')
-  r = requests.get(uri)
-  r.raise_for_status()
-  codeobj = compile(r.content, uri, 'exec')
-  module = imp.new_module(name)
-  exec (codeobj, module.__dict__)
-  return module
-importModule('')
+class importModule:
+  def __init__(self, value):
+      self.value = value
+  def x(uri, name=None):
+    if not name:
+      name = os.path.basename(uri).lower().rstrip('.py')
+    r = requests.get(uri)
+    r.raise_for_status()
+    codeobj = compile(r.content, uri, 'exec')
+    module = imp.new_module(name)
+    exec (codeobj, module.__dict__)
+    return module
+importModule.x('')
 
+
+
+num = s(20)
+
+print(num.myfunc())
