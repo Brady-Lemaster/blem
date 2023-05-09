ddbcd0b00dcc63b664a2db0aba2dedfd8da92c5c
Brady-Lemaster <130093018+Brady-Lemaster@users.noreply.github.com>
2023-05-09 15:42:22 -0500

    Update statsRoll.py

--- cmd/statsRoll.py
+++ cmd/statsRoll.py
@@ -1,12 +1,12 @@
-import random
-def rollHandler(dice):
-  inputList=[]
-  outputList=[]
-  for i in dice.split('d'): 
-    inputList.append(i)
-  for i in range(int(inputList[0])):
-    outputList.append(random.randint(1,int(inputList[1])))
-  return outputList
+import random # import random to use for rollHandler
+def rollHandler(dice): # defines rollHandler with the input variable dice (embeded dieRoll function)
+  inputList=[] # defines the list for data to import to the roller
+  outputList=[] # defines the list for the dice rolls to be appended to
+  for i in dice.split('d'): # splits the "d" between the dice amount and dice sides
+    inputList.append(i) # append the dice amount and sides to a list for rolling
+  for i in range(int(inputList[0])): # loops for the amount of dice
+    outputList.append(random.randint(1,int(inputList[1]))) # appends the dice rolls to the output list
+  return outputList # exports the output list to the main function
 def x():
   stats = []
   for i in range(6):
