13a0ebef3a3841a497d0fc8ab3b88c908d512f57
Brady-Lemaster <130093018+Brady-Lemaster@users.noreply.github.com>
2023-05-09 15:27:52 -0500

    Update dieSampler.py

--- cmd/dieSampler.py
+++ cmd/dieSampler.py
@@ -10,9 +10,9 @@ def rollHandler(dice): # defines rollHandler with the input variable dice (embed
     outputList.append(random.randint(1,int(inputList[1]))) # appends the dice rolls to the output list
   return outputList # exports the output list to the main function
 def x(dice, file): # defines the main function with the rolled dice and the export file
-  rolledDiceX = rollHandler(dice)
-  resultX = np.array(rolledDiceX)
-  resultY = random.randint(1,int(dice.split("d")[0]))
-  plt.bar(resultX, resultY)
-  plt.savefig(file)
+  rolledDiceX = rollHandler(dice) # rolls dice for the data to add to the graph
+  resultX = np.array(rolledDiceX) # converts rhe data to a numpy array
+  resultY = random.randint(1,int(dice.split("d")[0])) # randomly picks between the amount of dice rolled and 1 for graph
+  plt.bar(resultX, resultY) # adds the data (resultX) and the amount of rolls in a section (resultY)
+  plt.savefig(file) # saves to the designated file
   
