32fb404debeb87774277698e738ed259d993709b
Brady-Lemaster <130093018+Brady-Lemaster@users.noreply.github.com>
2023-05-09 15:46:27 -0500

    Create package.py

---
+++ packages/package.py
@@ -0,0 +1,14 @@
+import os
+import requests
+import types
+class default.importModule:
+  def x(uri, name=None):
+    if not name:
+      name = os.path.basename(uri).lower().rstrip('.py')
+    r = requests.get(uri)
+    r.raise_for_status()
+    codeobj = compile(r.content, uri, 'exec')
+    module = types.ModuleType(name)
+    exec (codeobj, module.__dict__)
+    return module
+# diceUtil = importModule.x('https://raw.githubusercontent.com/Brady-Lemaster/blem/main/package/dicePkg.py')
