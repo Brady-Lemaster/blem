4065bf37543f0c9a45fdeafef04abfa2e4cb06ad
Brady-Lemaster <130093018+Brady-Lemaster@users.noreply.github.com>
2023-05-08 21:41:29 -0500

    Update dieSampler.py

--- cmd/dieSampler.py
+++ cmd/dieSampler.py
@@ -1,8 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import random
+def rollHandler(dice):
+  inputList=[]
+  outputList=[]
+  for i in dice.split('d'): 
+    inputList.append(i)
+  for i in range(int(inputList[0])):
+    outputList.append(random.randint(1,int(inputList[1])))
+  return outputList
 def x(dice, file):
-  rolledDiceX = blem.dieRoll.x(dice)
-  rolledDiceY = blem.dieRoll.x(dice)
+  rolledDiceX = rollHandler(dice)
+  rolledDiceY = rollHandler(dice)
   numpyDiceX = np.array(rolledDiceX)
   numpyDiceY = np.array(rolledDiceY)
   plt.bar(numpyDiceX, numpyDiceY)
