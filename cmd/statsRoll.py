5045d276d39260e21dc2afa924846f4575a22ebc
Brady-Lemaster <130093018+Brady-Lemaster@users.noreply.github.com>
2023-05-09 15:40:39 -0500

    Update statsRoll.py

--- cmd/statsRoll.py
+++ cmd/statsRoll.py
@@ -5,18 +5,13 @@ def rollHandler(dice):
   for i in dice.split('d'): 
     inputList.append(i)
   for i in range(int(inputList[0])):
-    outputList.append(random.randint(2,int(inputList[1])))
+    outputList.append(random.randint(1,int(inputList[1])))
   return outputList
 def x():
-  dice = rollHandler('4d6')
-  dice.sort()
-  dice.pop(0)
-  dice.pop(0)
-  rolledDice = []
-  rolledDice.append(dice.count(1))
-  rolledDice.append(dice.count(2))
-  rolledDice.append(dice.count(3))
-  rolledDice.append(dice.count(4))
-  rolledDice.append(dice.count(5))
-  rolledDice.append(dice.count(6))
-  return rolledDice
+  stats = []
+  for i in range(6):
+    dice = rollHandler('4d6')
+    dice.sort()
+    dice.pop(0)
+    stats.append(dice[0] + dice[1] + dice[2])
+  return stats
