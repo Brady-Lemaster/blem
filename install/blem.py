a5409fe8809aed0980e31fc2a571df62c9134caf
Brady-Lemaster <130093018+Brady-Lemaster@users.noreply.github.com>
2023-05-11 15:15:42 -0500

    Update blemStatic.py

--- install/blem.py
+++ install/blem.py
@@ -1,19 +1,6 @@
 import os
 import requests
 import types
-name = None
-uri = 'https://raw.githubusercontent.com/Brady-Lemaster/blem/main/package/package.py'
-types.ModuleType('name')
-if not name:
-  name = os.path.basename(uri).lower().rstrip('.py')
-r = requests.get(uri)
-r.raise_for_status()
-codeobj = compile(r.content, uri, 'exec')
-module = types.ModuleType(name)
-exec (codeobj, module.__dict__)
-import os
-import requests
-import types
 class importModule:
   def x(uri, name=None):
     if not name:
