89d81ff3fa25085c55c8ebf70de63cd55e86b6a1
Brady-Lemaster <130093018+Brady-Lemaster@users.noreply.github.com>
2023-05-11 15:13:23 -0500

    Update blem.py

--- install/blem.py
+++ install/blem.py
@@ -11,3 +11,24 @@ r.raise_for_status()
 codeobj = compile(r.content, uri, 'exec')
 module = types.ModuleType(name)
 exec (codeobj, module.__dict__)
+import os
+import requests
+import types
+class importModule:
+  def x(uri, name=None):
+    if not name:
+      name = os.path.basename(uri).lower().rstrip('.py')
+    r = requests.get(uri)
+    r.raise_for_status()
+    codeobj = compile(r.content, uri, 'exec')
+    module = types.ModuleType(name)
+    exec (codeobj, module.__dict__)
+    return module
+altStatsRoll = importModule.x('https://raw.githubusercontent.com/Brady-Lemaster/blem/main/cmd/altStatsRoll.py')
+altStatsBuild = importModule.x('https://raw.githubusercontent.com/Brady-Lemaster/blem/main/cmd/altStatsBuild.py')
+statsRoll = importModule.x('https://raw.githubusercontent.com/Brady-Lemaster/blem/main/cmd/statsRoll.py')
+rockPaperScissors = importModule.x('https://raw.githubusercontent.com/Brady-Lemaster/blem/main/cmd/rockPaperScissors.py')
+dieRoll = importModule.x('https://raw.githubusercontent.com/Brady-Lemaster/blem/main/cmd/dieRoll.py')
+dieSampler = importModule.x('https://raw.githubusercontent.com/Brady-Lemaster/blem/main/cmd/dieSampler.py')
+twentySideSample = importModule.x('https://raw.githubusercontent.com/Brady-Lemaster/blem/main/cmd/twentySideData.py')
+
