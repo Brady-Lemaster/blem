2160f9d3a16e27e40e6469dd8fe8b4b4e7fedce1
Brady-Lemaster <130093018+Brady-Lemaster@users.noreply.github.com>
2023-05-12 23:43:53 -0500

    Update chatManager.py

--- cmd/chatManager.py
+++ cmd/chatManager.py
@@ -1,15 +1,15 @@
-def x(profile, element, content):
-  if element == "model":
-    profile[0] = content
-  elif element == "random":
-    profile[1] = content
-  elif element == "limit":
-    profile[2] = content
-  elif element == "key":
-    profile[3] = content
-  else:
-    print("ERROR: ELEMENT NOT FOUND")
-  if profile == None:
-    return(["ada", 50, "placeholderKey"])
-  return(profile)
+def x(profile, element, content): # the function with an option for the profile list, element to change, and content to add
+  if profile == None: # if there isnt a profile to use
+    profile = ["ada", 1, 1000, "placeholderKey"] # uses a prebuilt for these purposes
+  if element == "model": # checks for element model
+    profile[0] = content # changes the model element to content
+  elif element == "random": # checks for element random
+    profile[1] = content # changes the random element to content
+  elif element == "limit": # checks for element limit
+    profile[2] = content # changes the limit element to content
+  elif element == "key": # checks for element key
+    profile[3] = content # changes the key element to content
+  else: # if theres no element of the name
+    print("ERROR: ELEMENT NOT FOUND") # prints an error
+  return(profile) # returns the modified profile
  
