f58b9818538490d43b3c1cdc9795743f9c6438c4
Brady-Lemaster <130093018+Brady-Lemaster@users.noreply.github.com>
2023-05-13 20:19:36 -0500

    Update blemStatic.py

--- install/blemStatic.py
+++ install/blemStatic.py
@@ -112,3 +112,25 @@ class imageManager:
 class imageProfile:
   def x():
     return ["256x256", 1, "placeholderKey"]
+class rockPaperScissors:
+  def x(elements, user):
+    elementsFigure = elements+elements
+    if elements.count(user) > 0:
+      botChosen = round(random.randint(1, len(elements)))
+      botChosenIndex = botChosen - 1
+      if elementsFigure[botChosenIndex - 1] == user:
+        print("bot: " + elements[botChosenIndex])
+        print("WIN")
+        print(botChosenIndex)
+      elif elementsFigure[botChosenIndex + 1] == user: 
+        print("bot: " + elements[botChosenIndex])
+        print("LOSE")
+        print(botChosenIndex)
+      else:
+        print("bot: " + elements[botChosenIndex])
+        print("TIE")
+        print(botChosenIndex)
+    else:
+      print("ERROR: " + user + " ELEMENT DOES NOT EXIST")
+      print("LOSE")
+      print("botChosenIndex")
