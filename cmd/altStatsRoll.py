6929ef95af6f5df3201f708f3a40096dd6fbf1e7
Brady-Lemaster <130093018+Brady-Lemaster@users.noreply.github.com>
2023-05-13 19:30:08 -0500

    Update altStatsRoll.py

--- cmd/altStatsRoll.py
+++ cmd/altStatsRoll.py
@@ -1,5 +1,5 @@
 import random # import random to use for rollHandler
-def rollHandler(dice): # defines rollHandler with the input variable dice (embeded dieRoll function)
+def rollHandler(dice): # defines rollHandler with the input variable dice (embedded dieRoll function)
   inputList=[] # defines the list for data to import to the roller
   outputList=[] # defines the list for the dice rolls to be appended to
   for i in dice.split('d'): # splits the "d" between the dice amount and dice sides
