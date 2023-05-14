e0890b174ba0c7fdfcebddb0d2bff49b0512e383
Brady-Lemaster <130093018+Brady-Lemaster@users.noreply.github.com>
2023-05-13 19:21:54 -0500

    Update altStatsBuild.py

--- cmd/altStatsBuild.py
+++ cmd/altStatsBuild.py
@@ -1,4 +1,4 @@
-def statsBuild(rolledDice, die1, die2, die3): # define the function with a rolled set of dice and the dice to use as inputs
+def x(rolledDice, die1, die2, die3): # define the function with a rolled set of dice and the dice to use as inputs
   if rolledDice[die1-1] > 0 and rolledDice[die2-1] > 0 and rolledDice[die3-1] > 0: # checks if theres enough dice
     rolledDice[die1-1] = rolledDice[die1-1]-1 # if so subtracts the first die from the dataset
     rolledDice[die2-1] = rolledDice[die2-1]-1 # then the second
