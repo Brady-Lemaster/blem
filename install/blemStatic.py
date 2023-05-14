2d6c9ad39e1afb0fa739411ea00e175a1206d499
Brady-Lemaster <130093018+Brady-Lemaster@users.noreply.github.com>
2023-05-13 19:34:27 -0500

    Update blemStatic.py

--- install/blemStatic.py
+++ install/blemStatic.py
@@ -1,1 +1,47 @@
+import os
+import requests
+import types
+import random 
+class roll
+def rollHandler(dice):  inputList=[] 
+  outputList=[] 
+  for i in dice.split('d'): 
+    inputList.append(i) 
+  for i in range(int(inputList[0])):
+    outputList.append(random.randint(1,int(inputList[1]))) 
+  return outputList 
+class importModule:
+  def x(uri, name=None):
+    if not name:
+      name = os.path.basename(uri).lower().rstrip('.py')
+    r = requests.get(uri)
+    r.raise_for_status()
+    codeobj = compile(r.content, uri, 'exec')
+    module = types.ModuleType(name)
+    exec (codeobj, module.__dict__)
+    return module
+class altStatsBuild:
+  def x(rolledDice, die1, die2, die3): 
+    if rolledDice[die1-1] > 0 and rolledDice[die2-1] > 0 and rolledDice[die3-1] > 0:
+      rolledDice[die1-1] = rolledDice[die1-1]-1
+      rolledDice[die2-1] = rolledDice[die2-1]-1 
+      rolledDice[die3-1] = rolledDice[die3-1]-1
+      stat = die1 + die2 + die3 
+      return(rolledDice, stat) 
+    else:
+      return(rolledDice, "error") 
+class altStatsRoll:
+  def x():
+    dice = rollHandler('20d6')
+    dice.sort()
+    dice.pop(0)
+    dice.pop(0)
+    rolledDice = []
+    rolledDice.append(dice.count(1))
+    rolledDice.append(dice.count(2))
+    rolledDice.append(dice.count(3))
+    rolledDice.append(dice.count(4))
+    rolledDice.append(dice.count(5))
+    rolledDice.append(dice.count(6))
+    return rolledDice
 
