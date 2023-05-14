453ba32287b10d4aef9cb4facaf6cf60d81b66fc
Brady-Lemaster <130093018+Brady-Lemaster@users.noreply.github.com>
2023-05-13 19:35:20 -0500

    Update blemStatic.py

--- install/blemStatic.py
+++ install/blemStatic.py
@@ -2,14 +2,14 @@ import os
 import requests
 import types
 import random 
-class roll
-def rollHandler(dice):  inputList=[] 
-  outputList=[] 
-  for i in dice.split('d'): 
-    inputList.append(i) 
-  for i in range(int(inputList[0])):
-    outputList.append(random.randint(1,int(inputList[1]))) 
-  return outputList 
+class dieRoll:
+  def x(dice):  inputList=[] 
+    outputList=[] 
+    for i in dice.split('d'): 
+      inputList.append(i) 
+    for i in range(int(inputList[0])):
+      outputList.append(random.randint(1,int(inputList[1]))) 
+    return outputList 
 class importModule:
   def x(uri, name=None):
     if not name:
