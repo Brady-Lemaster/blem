73b335b163ef0967f9fab562f7971f1d885e3e3d
Brady-Lemaster <130093018+Brady-Lemaster@users.noreply.github.com>
2023-05-15 10:33:26 -0500

    Update blemStatic.py

--- install/blemStatic.py
+++ install/blemStatic.py
@@ -150,4 +150,13 @@ class twentySideData:
     for i in range(20):
       diceAmount.append(dice.count(i+1))   
     return(diceAmount)
-  
+class chatbot3:
+  def x(prompt, chatHistory, key):
+    openai.api_key = key
+    chatHistory.append({"role": "user", "content": prompt})
+    response = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=chatHistory
+    )['choices'][0]['message']['content']
+    chatHistory.append({"role": "assistant", "content": response})
+    return response, chatHistory
